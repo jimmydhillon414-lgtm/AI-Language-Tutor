375233b79a3aeec82da4254db0ac65507ef36179
jimmydhillon414-lgtm <jimmydhillon414@gmail.com>
2026-09-05 13:06:00 +0530

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import Navbar from '../components/Navbar';
 import LoginScreen from '../screens/LoginScreen';
 import AuthScreen from '../screens/AuthScreen';
 
@@ -70,14 +69,6 @@ export default function AppNavigator() {
 
   return (
     <View style={styles.container}>
-      <Navbar 
-        user={user} 
-        activeTab={activeTab} 
-        setActiveTab={setActiveTab} 
-        onOpenLogin={() => { setAuthMode('login'); setShowAuthModal(true); }}
-        onOpenSignup={() => { setAuthMode('signup'); setShowAuthModal(true); }}
-        onSignOut={handleSignOut}
-      />
       <View style={styles.content}>
         {renderContent()}
       </View>
